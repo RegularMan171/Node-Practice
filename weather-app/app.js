8693a3c8f6216a6c9bf9857f685f937535ebbcae
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-02-07 17:51:09 +0530

    added error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('postman-request');
 const config = require('./config');
 
-request({ url: config.url }, (error, response) => {
+// request({ url: config.url }, (error, response) => {
+//     const data = JSON.parse(response.body);
+//     if(error) {
+//         console.log('Cannot connect to weather service');
+//     } else if (data.error) {
+//         console.log('could not find the location');
+//     } else {
+//         console.log(data.current.temperature, data.current.precip);
+//     }
+// });
+
+request({ url: config.mapbox_url }, (error, response) => {
     const data = JSON.parse(response.body);
-    console.log(data.current);
+    if(error) {
+        console.log("Unable to connect to location services");
+    } else if(data.features.length === 0) {
+        console.log("Unable to find location, Try again")
+    } else {
+        console.log('Longitude: '+data.features[0].center[0]+' Latitute: '+ data.features[0].center[1])
+    }
 })
