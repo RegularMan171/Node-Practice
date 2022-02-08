fbb3f90a5ec6cfaefa8b97fe0641bdb158f1de8a
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-02-08 18:46:28 +0530

    added handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,8 +4,30 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: "Name"
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: "Not my name"
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpMessage: "Help is coming"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
