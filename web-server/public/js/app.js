4e768639284f528d3570d182647f1965719c19aa
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-02-10 00:38:54 +0530

    fetch() data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
-console.log('client side js is loaded')
+console.log('client side js is loaded');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     });
+// });
+
+fetch('http://localhost:3000/weather?address=Hyderabad').then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location, data.forecast)
+        }
+    })
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,14 +47,14 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if(error) {
-            res.send({ error })
+            return res.send({ error })
         }
 
         forecast(latitude, longitude, (error, forecastData) => {
             if(error) {
-                res.send({ error })
+                return res.send({ error })
             }
             res.send({
                 forecast: forecastData,
