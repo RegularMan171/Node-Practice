e83013cfa7018bcdbd34a9da549d49eae99b9978
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-02-10 01:07:46 +0530

    before deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,26 @@
-console.log('client side js is loaded');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#msg-1');
+const messageTwo = document.querySelector('#msg-2');
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     });
-// });
+weatherForm.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+    
+    const location = search.value;
 
-fetch('http://localhost:3000/weather?address=Hyderabad').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location, data.forecast)
-        }
-    })
-})
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else {
+                console.log(data.location, data.forecast)
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });})
