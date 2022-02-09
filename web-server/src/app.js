24c8fa8e4ea082ca064e2301edff7f11cea90fe1
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-02-09 23:10:56 +0530

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,11 +39,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
     res.send([{
         forecast: "It's Sunny",
-        location: "Hyderabad"
+        location: "Hyderabad",
+        address: req.query.address
     }]);
 });
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products:[]
+    });
+})
 
 app.get('/help/*', (req, res) => {
     res.render('notfound', {
